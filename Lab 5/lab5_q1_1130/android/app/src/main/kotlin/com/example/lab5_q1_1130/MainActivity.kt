c2d33a66d40f1069b4c406a81099ff9580ae9f6c
Wasi Ur Rehman Qamar <76022253+WasiUrRehmanQamar@users.noreply.github.com>
2022-04-03 00:40:30 +0500

    Lab 5

---
+++ Lab 5/lab5_q1_1130/android/app/src/main/kotlin/com/example/lab5_q1_1130/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lab5_q1_1130
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Lab 5/lab5_q2_1130/android/app/src/main/kotlin/com/example/lab5_q2_1130/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lab5_q2_1130
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
