e2a84f162e00808d8a1631d90e461788f726bd79
Wasi Ur Rehman Qamar <76022253+WasiUrRehmanQamar@users.noreply.github.com>
2022-04-05 04:37:01 +0500

    Final

--- Lab 6/lab6_wasi_1130/android/app/src/main/kotlin/com/example/lab6_wasi_1130/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.lab6_wasi_1130
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Lab 6/lab6_wasi_1130/lib/QuestionList.dart
+++
@@ -1,9 +0,0 @@
-// ignore_for_file: non_constant_identifier_names
-
-class Questions {
-  String QuestionString = '';
-
-  bool Answer = false;
-
-  Questions(this.QuestionString, this.Answer);
-}
